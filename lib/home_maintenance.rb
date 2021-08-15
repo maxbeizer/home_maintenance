fd6a90971de066c3a7daaaf25b424aee4604062d
Max Beizer <max.beizer@gmail.com>
2021-08-15 12:41:05 -0500

    Start adding main interface

--- lib/home_maintenance.rb
+++ lib/home_maintenance.rb
@@ -1,9 +1,32 @@
 # frozen_string_literal: true
 
+require 'csv'
+
 require_relative './home_maintenance/version'
 require_relative './home_maintenance/task'
 
+# Read data on tasks. Convert them to task objects.
 class HomeMaintenance
   class Error < StandardError; end
-  # Your code goes here...
+  DEFAULT_DATA_PATH = './data/tasks.csv'
+
+  def self.call(path_to_data = DEFAULT_DATA_PATH)
+    new(path_to_data).call
+  end
+
+  def initialize(path_to_data = DEFAULT_DATA_PATH)
+    @path_to_data = path_to_data
+  end
+
+  def call
+    build_task_objects
+  end
+
+  private
+
+  def build_task_objects
+    CSV.read(@path_to_data).each_with_object([]) do |row, acc|
+      acc << Task.new(row).call
+    end.compact
+  end
 end
--- test/home_maintenance_test.rb
+++ test/home_maintenance_test.rb
@@ -7,7 +7,27 @@ class HomeMaintenanceTest < Minitest::Test
     refute_nil ::HomeMaintenance::VERSION
   end
 
-  def test_it_does_something_useful
-    refute false
+  def test_it_returns_a_list_of_task_like_objects
+    subject_class.call.each do |res|
+      assert_respond_to res, :season
+      assert_respond_to res, :area
+      assert_respond_to res, :task_type
+      assert_respond_to res, :task_name
+    end
+  end
+
+  # NOTE: this is dependent on the actual data in the CSV
+  def test_it_returns_data_as_expected
+    result = subject_class.call.first
+    assert_equal 'Fall', result.season
+    assert_equal 'Appliances', result.area
+    assert_equal 'Cleaning', result.task_type
+    assert_equal 'Clean AC condensor', result.task_name
+  end
+
+  private
+
+  def subject_class
+    HomeMaintenance
   end
 end
