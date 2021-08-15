54d6dd69052c9e4ee48e710bb3d244c7ab43e805
Max Beizer <max.beizer@gmail.com>
2021-08-15 14:09:08 -0500

    Add in octokit implementation

--- lib/home_maintenance.rb
+++ lib/home_maintenance.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'csv'
+require 'octokit'
 
 require_relative './home_maintenance/version'
 require_relative './home_maintenance/task'
@@ -16,16 +17,27 @@ class HomeMaintenance
   end
 
   def initialize(config = {})
+    @issue_client = config[:issue_client] || Octokit::Client.new(access_token: config[:github_token])
     @task_class = config[:task_class] || Task
     @time_framer_class = config[:time_framer_class] || TimeFramer
 
+    @repo_nwo = config[:repo_nwo]
     @path_to_data = config[:path_to_data] || DEFAULT_DATA_PATH
     @time_frame = config[:time_frame] || :seasonal
     @run_date = config[:run_date] || Date.today
   end
 
   def call
-    build_task_objects
+    build_task_objects.map do |task|
+      @issue_client.create_issue(
+        @repo_nwo,
+        task.task_name,
+        '**created by https://github.com/maxbeizer/home_maintenance**',
+        labels: ['home_maintenance', task.area, task.task_type].join(',')
+      )
+
+      task
+    end
   end
 
   private
